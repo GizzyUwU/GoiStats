b5cbbaf4f615618d20a9810539923804eca0a0a0
gizzy <nerop455@gmail.com>
2026-09-11 08:15:01 +0100

    fix: needs metaprovider

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'solid-js/web'
 import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
 import './index.css'
 import { App } from './App'
+import { MetaProvider } from '@solidjs/meta'
 
 const root = document.getElementById('root')
 const queryClient = new QueryClient()
@@ -11,7 +12,9 @@ if (root) {
   render(
     () => (
       <QueryClientProvider client={queryClient}>
-        <App />
+            <MetaProvider>
+          <App />
+            </MetaProvider>
       </QueryClientProvider>
     ),
     root,
